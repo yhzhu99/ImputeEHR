ca474faabdb65ed6966906a2fd8203c0f2f572bb
Chenrui <2399891646@qq.com>
2023-07-07 12:23:16 +0800

    feat: Added imperfect PCA

--- examples.py
+++ examples.py
@@ -11,8 +11,10 @@ random_seed = 42
 # split dataset into train, test, val set
 train_size, test_size, val_size = 0.7, 0.1, 0.2
 ds = pd.read_csv("./datasets/example.csv")
-train_ds, val_test_ds = train_test_split(ds, train_size=train_size, random_state=random_seed)
-val_ds, test_ds = train_test_split(val_test_ds, train_size=val_size/(val_size+test_size), random_state=random_seed)
+train_ds, val_test_ds = train_test_split(
+    ds, train_size=train_size, random_state=random_seed)
+val_ds, test_ds = train_test_split(
+    val_test_ds, train_size=val_size/(val_size+test_size), random_state=random_seed)
 
 # print dataset size
 print("Samples of training set:", len(train_ds))
@@ -24,6 +26,28 @@ method = "ZeroImpute"
 
 ########################
 # 1. Zero Imputation
+########################
+
+# init the pipeline
+pipeline = getattr(impute_ehr, method)(train_ds)
+# fit the imputation model if required
+if pipeline.require_fit:
+    pipeline.fit()
+# execute the imputation pipeline
+train_ds_imputed = pipeline.execute(train_ds)
+val_ds_imputed = pipeline.execute(val_ds)
+test_ds_imputed = pipeline.execute(test_ds)
+# save the imputed dataset
+Path(f"./datasets/imputed/{method}").mkdir(parents=True, exist_ok=True)
+train_ds_imputed.to_csv(f"./datasets/imputed/{method}/train.csv", index=False)
+val_ds_imputed.to_csv(f"./datasets/imputed/{method}/val.csv", index=False)
+test_ds_imputed.to_csv(f"./datasets/imputed/{method}/test.csv", index=False)
+
+# execute imputation pipeline
+method = "PCAImpute"
+
+########################
+# 3. PCA Imputation
 ########################
 
 # init the pipeline
