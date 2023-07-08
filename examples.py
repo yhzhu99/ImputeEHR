00f33cdaa9ea03f5effb8fc554f6fd22b3cfa41d
Yinghao Zhu <yhzhu99@gmail.com>
2023-07-08 16:13:00 +0800

    chore: update test script, use for loop for each method

--- examples.py
+++ examples.py
@@ -21,106 +21,21 @@ print("Samples of training set:", len(train_ds))
 print("Samples of validation set:", len(val_ds))
 print("Samples of test set:", len(test_ds))
 
-# execute imputation pipeline
-method = "ZeroImpute"
+methods = ["ZeroImpute", "KNNImpute", "PCAImpute", "RandomForestImpute", "MICEImpute"]
 
-########################
-# 1. Zero Imputation
-########################
-
-# init the pipeline
-pipeline = getattr(impute_ehr, method)(train_ds)
-# fit the imputation model if required
-if pipeline.require_fit:
-    pipeline.fit()
-# execute the imputation pipeline
-train_ds_imputed = pipeline.execute(train_ds)
-val_ds_imputed = pipeline.execute(val_ds)
-test_ds_imputed = pipeline.execute(test_ds)
-# save the imputed dataset
-Path(f"./datasets/imputed/{method}").mkdir(parents=True, exist_ok=True)
-train_ds_imputed.to_csv(f"./datasets/imputed/{method}/train.csv", index=False)
-val_ds_imputed.to_csv(f"./datasets/imputed/{method}/val.csv", index=False)
-test_ds_imputed.to_csv(f"./datasets/imputed/{method}/test.csv", index=False)
-
-
-########################
-# 2. KNN Imputation
-########################
-
-method = "KNNImpute"
-# init the pipeline
-pipeline = getattr(impute_ehr, method)(train_ds)
-# fit the imputation model if required
-if pipeline.require_fit:
-    pipeline.fit()
-# execute the imputation pipeline
-train_ds_imputed = pipeline.execute(train_ds)
-val_ds_imputed = pipeline.execute(val_ds)
-test_ds_imputed = pipeline.execute(test_ds)
-# save the imputed dataset
-Path(f"./datasets/imputed/{method}").mkdir(parents=True, exist_ok=True)
-train_ds_imputed.to_csv(f"./datasets/imputed/{method}/train.csv", index=False)
-val_ds_imputed.to_csv(f"./datasets/imputed/{method}/val.csv", index=False)
-test_ds_imputed.to_csv(f"./datasets/imputed/{method}/test.csv", index=False)
-
-
-########################
-# 3. PCA Imputation
-########################
-
-method = "PCAImpute"
-# init the pipeline
-pipeline = getattr(impute_ehr, method)(train_ds)
-# fit the imputation model if required
-if pipeline.require_fit:
-    pipeline.fit()
-# execute the imputation pipeline
-train_ds_imputed = pipeline.execute(train_ds)
-val_ds_imputed = pipeline.execute(val_ds)
-test_ds_imputed = pipeline.execute(test_ds)
-# save the imputed dataset
-Path(f"./datasets/imputed/{method}").mkdir(parents=True, exist_ok=True)
-train_ds_imputed.to_csv(f"./datasets/imputed/{method}/train.csv", index=False)
-val_ds_imputed.to_csv(f"./datasets/imputed/{method}/val.csv", index=False)
-test_ds_imputed.to_csv(f"./datasets/imputed/{method}/test.csv", index=False)\
-
-########################
-# 4. RandomForest Imputation
-########################
-
-method = "RandomForestImpute"
-# init the pipeline
-pipeline = getattr(impute_ehr, method)(train_ds)
-# fit the imputation model if required
-if pipeline.require_fit:
-    pipeline.fit()
-# execute the imputation pipeline
-train_ds_imputed = pipeline.execute(train_ds)
-val_ds_imputed = pipeline.execute(val_ds)
-test_ds_imputed = pipeline.execute(test_ds)
-# save the imputed dataset
-Path(f"./datasets/imputed/{method}").mkdir(parents=True, exist_ok=True)
-train_ds_imputed.to_csv(f"./datasets/imputed/{method}/train.csv", index=False)
-val_ds_imputed.to_csv(f"./datasets/imputed/{method}/val.csv", index=False)
-test_ds_imputed.to_csv(f"./datasets/imputed/{method}/test.csv", index=False)
-
-########################
-# 5. MICE Imputation
-########################
-
-method = "MICEImpute"
-# init the pipeline
-pipeline = getattr(impute_ehr, method)(train_ds)
-# fit the imputation model if required
-if pipeline.require_fit:
-    pipeline.fit()
-# execute the imputation pipeline
-train_ds_imputed = pipeline.execute(train_ds)
-val_ds_imputed = pipeline.execute(val_ds)
-test_ds_imputed = pipeline.execute(test_ds)
-# save the imputed dataset
-Path(f"./datasets/imputed/{method}").mkdir(parents=True, exist_ok=True)
-train_ds_imputed.to_csv(f"./datasets/imputed/{method}/train.csv", index=False)
-val_ds_imputed.to_csv(f"./datasets/imputed/{method}/val.csv", index=False)
-test_ds_imputed.to_csv(f"./datasets/imputed/{method}/test.csv", index=False)
+for method in methods:
+    # execute imputation pipeline
+    # init the pipeline
+    pipeline = getattr(impute_ehr, method)(train_ds)
+    # fit the imputation model if required
+    if pipeline.require_fit:
+        pipeline.fit()
+    # execute the imputation pipeline
+    train_ds_imputed = pipeline.execute(train_ds)
+    val_ds_imputed = pipeline.execute(val_ds)
+    test_ds_imputed = pipeline.execute(test_ds)
+    # save the imputed dataset
+    Path(f"./datasets/imputed/{method}").mkdir(parents=True, exist_ok=True)
+    train_ds_imputed.to_csv(f"./datasets/imputed/{method}/train.csv", index=False)
+    val_ds_imputed.to_csv(f"./datasets/imputed/{method}/val.csv", index=False)
+    test_ds_imputed.to_csv(f"./datasets/imputed/{method}/test.csv", index=False)
