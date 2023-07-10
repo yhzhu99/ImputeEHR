8586e3ac7e8c3fc9f4803c7c7b3d81fa31212ea7
Yinghao Zhu <yhzhu99@gmail.com>
2023-07-11 01:37:29 +0800

    chore: save imputation model instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+from impute_ehr import models
 from impute_ehr.data import preprocess
 
 df = pd.read_csv("./datasets/example.csv")
@@ -12,3 +13,19 @@ impute_val_x = pd.read_pickle('./datasets/impute_val_x.pkl')
 impute_train_x_flat = preprocess.flatten_to_matrix(impute_train_x)
 impute_val_x_flat = preprocess.flatten_to_matrix(impute_val_x)
 
+# print dataset size
+print("Samples of training set:", len(impute_train_x_flat))
+print("Samples of validation set:", len(impute_val_x_flat))
+
+# methods = ["ZeroImpute", "KNNImpute", "PCAImpute", "RandomForestImpute", "MICEImpute"]
+methods = ["ZeroImpute"]
+
+for method in methods:
+    # execute imputation pipeline
+    # init the pipeline
+    pipeline = getattr(models, method)(impute_train_x_flat, impute_val_x_flat)
+    # fit the imputation model if required
+    if pipeline.require_fit:
+        pipeline.fit()
+    # save the imputation model
+    # TODO: save the model
