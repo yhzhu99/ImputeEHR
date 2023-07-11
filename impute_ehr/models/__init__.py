01eda519aecc63884ddbe92d52dc86e04a02b620
Yinghao Zhu <yhzhu99@gmail.com>
2023-07-12 03:40:45 +0800

    fix: __init__ import RFImpute

--- impute_ehr/models/__init__.py
+++ impute_ehr/models/__init__.py
@@ -2,6 +2,6 @@ from .knn import KNNImpute
 from .mice import MICEImpute
 from .nmf import NMFImpute
 from .pca import PCAImpute
-from .random_forest import RandomForestImpute
-from .soft_impute import SoftImpute
+from .rf import RFImpute
+from .soft import SoftImpute
 from .zero import ZeroImpute
